5744d092149283d755eecf6a73ea2e076026e070
linChunbin <Ben@linChunbindeMacBook-Pro.local>
2018-04-29 11:54:04 -0500

    dont print

--- ECE4220FinalProject/ECE4220FinalProject/main.cpp
+++ ECE4220FinalProject/ECE4220FinalProject/main.cpp
@@ -529,8 +529,8 @@ int main(int argc, const char * argv[]) {
     pthread_create(&adcReading, NULL, readingADC, NULL);
     pthread_create(&receiveThread, NULL, turnLEDS, NULL);
     while ( 1 ) {
-        r1.print();
-        s1.send();
+      //  r1.print();
+      //  s1.send();
         pullUpDnControl(BTN1,PUD_DOWN);//first set the push button's register down for input
         pullUpDnControl(BTN2,PUD_DOWN);//first set the push button's register down for input
         cout << eventCounter<<endl;
