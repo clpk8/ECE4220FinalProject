6a379f92f14edb73b339d9eeea6820f163be51c9
linChunbin <Ben@linChunbindeMacBook-Pro.local>
2018-04-29 12:54:57 -0500

    transfer

--- ECE4220FinalProject/ECE4220FinalProject/main.cpp
+++ ECE4220FinalProject/ECE4220FinalProject/main.cpp
@@ -470,7 +470,7 @@ void socketObj::setupSocket(){
 
 void socketObj::send(){
     LogData d1 = r1.getRTUData();
-    n = sendto(sock,d1.sendBuffer, 100, 0, (struct sockaddr *)&client, fromlen);
+    n = sendto(sock,&d1.sendBuffer, 100, 0, (struct sockaddr *)&client, fromlen);
     if(n < 0 )
         cout << "send error" << endl;
     else
