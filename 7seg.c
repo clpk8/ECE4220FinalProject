6ef1febbbf1ae2b61655a4483578af1fce7368a1
linChunbin <Ben@linChunbindeMacBook-Pro.local>
2018-05-02 22:41:11 -0500

    transfer

---
+++ 7seg.c
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <unistd.h>
+
+using namespace std;
+#define A 5
+#define B 6
+#define C 25
+#define D 2
+#define DP 4
+int main()
+{
+    cout << "Hello, World!";
+    if (wiringPiSetup () < 0) {
+        // fprintf (stderr, "Unable to setup wiringPi: %s\n", strerror (errno));
+        cerr<< "Not able to setup wiringpi"<<endl;
+        return -1;
+    }
+    
+    pinMode(A, OUTPUT);    // Configure GPIO2, which is the one connected to the red LED.
+    pinMode(B, OUTPUT);    // Configure GPIO2, which is the one connected to the red LED.
+    pinMode(C, OUTPUT);
+    pinMode(D, OUTPUT);
+    pinMode(DP, OUTPUT);
+    
+    pinMode(DP, HIGH);
+
+    
+    digitalWrite(A,HIGH);
+    digitalWrite(B,HIGH);
+    digitalWrite(C,HIGH);
+    digitalWrite(D,HIGH);
+    
+    sleep(5);
+    
+    digitalWrite(A,HIGH);
+    digitalWrite(B,LOW);
+    digitalWrite(C,LOW);
+    digitalWrite(D,LOW);
+    
+    sleep(5);
+    
+    digitalWrite(A,LOW);
+    digitalWrite(B,LOW);
+    digitalWrite(C,LOW);
+    digitalWrite(D,LOW);
+    sleep(5);
+    
+    return 0;
+}
