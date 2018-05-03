9b3a34c89ab0c7190ca61415a83f2f692191852c
linChunbin <Ben@linChunbindeMacBook-Pro.local>
2018-05-03 18:53:28 -0500

    transfer

--- client1.cpp
+++ client1.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <vector>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,12 +10,17 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <iostream>
+#include <algorithm>
+#include <sqlite3.h>
+#include <semaphore.h>
 
-#define MSG_SIZE 200			// message size
+sem_t semaphore;
+
+#define MSG_SIZE 200            // message size
 
 using namespace std;
 
-struct sockaddr_in anybody;	// for the socket configuration
+struct sockaddr_in anybody;    // for the socket configuration
 
 void error(const char *msg)
 {
@@ -22,34 +28,59 @@ void error(const char *msg)
     exit(0);
 }
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+    int i;
+    printf("\n");
+    return 0;
+}
+
+
+vector<int> ipID;
+
 // Receiving thr: constantly waits for messages. Whatever is received is displayed.
 void *receiving(void *ptr);
 
 int main(int argc, char *argv[])
 {
+    sem_init(&semaphore, 0, -1);
+    
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    char *sql;
+    
+    /* Open database */
+    rc = sqlite3_open("SCADA.db", &db);
+    if( rc ){
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        exit(0);
+    }else{
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    
     char signal[MSG_SIZE] = "2018-05-02 21:24:32|29|0|0|0|0|0|0|";
     int sock, n;
-    //  unsigned int length = sizeof(struct sockaddr_in);	// size of structure
-    //	char buffer[MSG_SIZE];		// to store received messages or messages to be sent.
-    //	LogData buffer;	// to store received messages or messages to be sent.
-    int boolval = 1;			// for a socket option
-    pthread_t thread_rec;		// thread variable
-
+    //  unsigned int length = sizeof(struct sockaddr_in);    // size of structure
+    //    char buffer[MSG_SIZE];        // to store received messages or messages to be sent.
+    //    LogData buffer;    // to store received messages or messages to be sent.
+    int boolval = 1;            // for a socket option
+    pthread_t thread_rec;        // thread variable
+    
     if(argc != 2)
     {
         cout << "usage: " << argv[0] << " port" << endl;
         exit(1);
     }
-
-    anybody.sin_family = AF_INET;		// symbol constant for Internet domain
-    anybody.sin_port = htons(atoi(argv[1]));				// port field
+    
+    anybody.sin_family = AF_INET;        // symbol constant for Internet domain
+    anybody.sin_port = htons(atoi(argv[1]));                // port field
     anybody.sin_addr.s_addr = htonl(INADDR_ANY);
-
+    
     sock = socket(AF_INET, SOCK_DGRAM, 0); // Creates socket. Connectionless.
     if(sock < 0)
         cerr << "Error: socket" << endl;
     //error("Error: socket");
-
+    
     int length = sizeof(anybody);
     bind(sock, (struct sockaddr *)&anybody, length);
     //         if (n < 0)
@@ -58,7 +89,7 @@ int main(int argc, char *argv[])
     //            //printf("Error binding socket.\n");
     //            exit(-1);
     //        }
-
+    
     // change socket permissions to allow broadcast
     if(setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &boolval, sizeof(boolval)) < 0)
     {
@@ -66,44 +97,46 @@ int main(int argc, char *argv[])
         //printf("Error setting socket options.\n");
         exit(-1);
     }
-
-    anybody.sin_addr.s_addr = inet_addr("128.206.19.255");	// broadcast address (Lab)
-
-    pthread_create(&thread_rec, NULL, receiving, (void *)&sock);	// for receiving
-
+    
+    
+    anybody.sin_addr.s_addr = inet_addr("128.206.19.255");    // broadcast address (Lab)
+    
+    pthread_create(&thread_rec, NULL, receiving, (void *)&sock);    // for receiving
+    
     cout << "This programs displays whatever it receives." << endl;
     cout << "It also transmits whatever the user types, max. 40 char. (! to exit):" << endl;
-
-    //printf("This programs displays whatever it receives.\n");
-    //printf("It also transmits whatever the user types, max. 40 char. (! to exit):\n");
-    do
-    {
-        if(signal[0] != '!')
-        {
-            // send message to anyone there...
-            n = sendto(sock, signal, strlen(signal), 0, (const struct sockaddr *)&anybody, length);
-            if(n < 0)
-                error("Error: sendto");
-        }
-
-        // bzero: to "clean up" the buffer. The messages aren't always the same length.
-        bzero(signal,MSG_SIZE);	// sets all values to zero. memset() could be used
-        //cleanbuffer(&buffer);
-        fgets(signal,MSG_SIZE-1,stdin); // MSG_SIZE-1 'cause a null character is added
-
-
-    } while(signal[0] != '!');
-
-    close(sock);			// close socket.
+    
+    
+    n = sendto(sock, signal, strlen(signal), 0, (const struct sockaddr *)&anybody, length);
+    if(n < 0)
+        error("Error: sendto");
+    
+    
+    sem_wait(&semaphore);
+    sprintf(signal,"%d|%d|%d|",ipID[0],ipID[1],atoi(argv[1]));
+    dummy = system("mkfifo N_pipe2");
+    
+    if((pipe_N_pipe2 = open("N_pipe2",O_WRONLY)) < 0){
+        printf("N_pipe2 error");
+        exit(-1);
+    }
+    
+    if(write(pipe_N_pipe2,&signal,sizeof(signal)) != sizeof(signal)){
+        printf("N_pipe2 writing error\n");
+        exit(-1);
+    }
+    
+    
+    //close(sock);            // close socket.
     return 0;
 }
 
 void *receiving(void *ptr)
 {
-    int *sock, n;
-    char buffer[MSG_SIZE];	// to store received messages or messages to be sent.
-
-    int EventID;
+    int *sock, n, i;
+    char buffer[MSG_SIZE];    // to store received messages or messages to be sent.
+    
+    int RTUID;
     int Switch1Status, Switch2Status, Button1Status, Button2Status;
     int Voltage;
     string EventOccuried;
@@ -111,93 +144,123 @@ void *receiving(void *ptr)
     int PowerFlag;
     char *value;
     int Event;
-
+    
     char delim[] = "|";
-
-    sock = (int *)ptr;		// socket identifier
-    unsigned int length = sizeof(struct sockaddr_in);		// size of structure
-
+    
+    sock = (int *)ptr;        // socket identifier
+    unsigned int length = sizeof(struct sockaddr_in);        // size of structure
+    
     struct sockaddr_in from;
-
+    
     while(1)
     {
-        bzero(buffer,MSG_SIZE);	// sets all values to zero. memset() could be used
+        bzero(buffer,MSG_SIZE);    // sets all values to zero. memset() could be used
         // receive message
-
+        
         n = recvfrom(*sock, buffer, 100, 0, (struct sockaddr *)&anybody, &length);
         if(n < 0)
             cerr << "Error: recvfrom" << endl;
         //error("Error: recvfrom");
-
+        
         //cout << "This was received: " << buffer << endl;
         //printf("This was received: %s\n", buffer);
-
+        
         value = strtok(buffer, delim);
         TimeStamp = value;
-
+        
         value = strtok(NULL, delim);
-        EventID = atoi(value);
-
+        RTUID = atoi(value);
+        
         value = strtok(NULL, delim);
         Switch1Status = atoi(value);
-
+        
         value = strtok(NULL, delim);
         Switch2Status = atoi(value);
-
+        
         value = strtok(NULL, delim);
         Button1Status = atoi(value);
-
+        
         value = strtok(NULL, delim);
         Button2Status = atoi(value);
-
+        
         value = strtok(NULL, delim);
         Voltage = atoi(value);
-
-
+        
+        
         value = strtok(NULL, delim);
         Event = atoi(value);
-
-//        cout << Event << endl << endl;
-
+        
+        if(find(ipID.begin(), ipID.end(), RTUID) == ipID.end()){
+            ipID.push_back(RTUID);
+            sem_post(&semaphore);
+        }
+        
+        
+        //        cout << Event << endl << endl;
+        
         switch(Event)
         {
             case 0: EventOccuried = "S10FF";
-                    break;
+                break;
             case 1: EventOccuried = "S1ON";
-                    break;
+                break;
             case 2: EventOccuried = "S2OFF";
-                    break;
+                break;
             case 3: EventOccuried = "S2ON";
-                    break;
+                break;
             case 4: EventOccuried = "B1OFF";
-                    break;
+                break;
             case 5: EventOccuried = "B1ON";
-                    break;
+                break;
             case 6: EventOccuried = "B2OFF";
-                    break;
+                break;
             case 7: EventOccuried = "B2ON";
-                    break;
+                break;
             case 8: EventOccuried = "ADCBOUND";
-                    break;
+                break;
             case 9: EventOccuried = "ADCPOWER";
-                    break;
+                break;
             case 10: EventOccuried = "LED1ON";
-                     break;
+                break;
             case 11: EventOccuried = "LED1OFF";
-                     break;
+                break;
             case 12: EventOccuried = "LED2ON";
-                     break;
+                break;
             case 13: EventOccuried = "LED2OFF";
-                     break;
+                break;
             case 14: EventOccuried = "REGULAR";
-                     break;
+                break;
         }
-
+        
         //value = strtok(NULL, delim);
         //PowerFlag = atoi(value);
-
-        cout << "This was received: " << TimeStamp << " " << EventID << " " << Switch1Status << " " << Switch2Status << " " << Button1Status << " " << Button2Status << " " << Voltage << " " << EventOccuried << endl;
+        
+        cout << "This was received: " << TimeStamp << " " << RTUID << " " << Switch1Status << " " << Switch2Status << " " << Button1Status << " " << Button2Status << " " << Voltage << " " << EventOccuried << endl;
+        cout << "Vector: " << ipID[i++] << endl;
+        
+        insert into RTUEventLog (TimeStamp, RTUID, Switch1Status, Switch2Status, Button1Status, Button2Status, Voltage, EventOccuried) values('2018-05-02 21:24:32', 20, 0,0,0,0,100, 'Test');
+        
+        sprintf(sql, "insert into insert into RTUEventLog (TimeStamp, RTUID, Switch1Status, Switch2Status, Button1Status, Button2Status, Voltage, EventOccuried) values('%s',%d,%d,%d,%d,%d,%d,'%s');",TimeStamp,RTUID,Switch1Status,Switch2Status,Button1Status,Button2Status,Voltage,EventOccuried);
+        
+        rc = sqlite3_exec(db, sql, callbacl, 0, &zErrMsg);
+        if( rc != SQLITE_OK ){
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+            sqlite3_free(zErrMsg);
+        }else{
+            fprintf(stdout, "Records created successfully\n");
+        }
+        
     }
-
+    
     pthread_exit(0);
 }
+
+
+
+
+
+
+
+
+
+
